c4a2869b225f0e06bb2f164cfb665eb8df80b0c8
Elle Ti <linh.htu@gmail.com>
2018-08-23 16:02:56 -0700

    Learned about Switch statements

--- PHPBasics.php
+++ PHPBasics.php
@@ -9,36 +9,22 @@
     <script src="main.js"></script>
 </head>
 <body>
-    <h3>PHP - The if Statement</h3>
+    <h3>The Switch Statement</h3>
     <?php
-    $t = date("H");
-    echo "The hour of the server is " . $t;
+    $favcolor = "red";
 
-    if ($t < "20") {
-        echo "<br>Have a good day!";
-    }
-    ?>
-    <h3>PHP - The if...else Statement</h3>
-    <?php
-    $t = date("H");
-
-    if ($t < "20") {
-        echo "Have a good day!";
-    }
-    else {
-        echo "Have a good night!";
-    }
-    ?>
-    <h3>PHP - The if...elseif....else Statement</h3>
-    <?php
-    $t = date("H");
-
-    if ($t < "10") {
-        echo "Have a good morning!";
-    } elseif ($t < "20") {
-        echo "Have a good day!";
-    } else {
-        echo "Have a good night!";
+    switch ($favcolor) {
+        case "red":
+            echo "Your favorite color is red";
+            break;
+        case "blue":
+            echo "Your favorite color is blue";
+            break;
+        case "green":
+            echo "Your favourite color is green";
+            break;
+        default:
+            echo "You don't have a favorite color";
     }
     ?>
 </body>
