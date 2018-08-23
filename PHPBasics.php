f35147718d1d7bab006b57fa66303581c75e7ec9
Elle Ti <linh.htu@gmail.com>
2018-08-23 15:48:25 -0700

    Learned about 'if..else..elseif' statements

--- PHPBasics.php
+++ PHPBasics.php
@@ -9,20 +9,37 @@
     <script src="main.js"></script>
 </head>
 <body>
-    <h3>Create a PHP Constant</h3>
+    <h3>PHP - The if Statement</h3>
     <?php
-    define("GREETING", "Welcome to W3Schools.com!", true); // true makes case insensitive
-    echo greeting;
-    ?>
-    <h3>Constants are Global</h3>
-    <?php
-    define("GREETING", "Welcome to W3Schools.com!", true);
+    $t = date("H");
+    echo "The hour of the server is " . $t;
 
-    function myTest() {
-        echo greeting;
+    if ($t < "20") {
+        echo "<br>Have a good day!";
     }
+    ?>
+    <h3>PHP - The if...else Statement</h3>
+    <?php
+    $t = date("H");
 
-    myTest();
+    if ($t < "20") {
+        echo "Have a good day!";
+    }
+    else {
+        echo "Have a good night!";
+    }
+    ?>
+    <h3>PHP - The if...elseif....else Statement</h3>
+    <?php
+    $t = date("H");
+
+    if ($t < "10") {
+        echo "Have a good morning!";
+    } elseif ($t < "20") {
+        echo "Have a good day!";
+    } else {
+        echo "Have a good night!";
+    }
     ?>
 </body>
 </html>
